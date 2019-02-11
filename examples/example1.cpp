ec4c90a08362232797ef24ca4a1aa0aa3dd47f72
Brandon T Cambio <btc6@ad.rice.edu>
2019-02-11 15:16:28 -0600

    deleted blank lines

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -7,11 +7,7 @@ int main(int argc, char const *argv[])
     std::vector<double> x = {1,2,3,4,5,};
     for (auto& i : x)
         std::cout << i << std::endl;
-
-<<<<<<< HEAD
         std::cout << "Hello, Evan" << std::endl;
-=======
-    std::cout << "Hello, Brandon" << std::endl;
->>>>>>> 08d34ca3c7e3e543cebf801ec2952318ab9a27fd
+
     return 0;
 }
