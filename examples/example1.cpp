364e342a2ec7595c8d38129dffb0c58b10b3a701
Brandon T Cambio <btc6@ad.rice.edu>
2019-02-11 15:07:46 -0600

    Added second string

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -7,5 +7,7 @@ int main(int argc, char const *argv[])
     std::vector<double> x = {1,2,3,4,5,};
     for (auto& i : x)
         std::cout << i << std::endl;
+
+        std::cout << "Hello, Evan" << std::endl;
     return 0;
 }
