ff547e8a041a251230b5f69ac4bdf7ef48d59964
Brandon T Cambio <btc6@ad.rice.edu>
2019-02-12 14:54:13 -0600

    testing

--- tests/evan_testing.cpp
+++ tests/evan_testing.cpp
@@ -24,8 +24,11 @@ int sawCallback(const void *inputBuffer, void *outputBuffer,
     for (i = 0; i < framesPerBuffer; i++)
     {
         *out++ = data->phase;  /* left */
+        *out++;
         /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+
         data->phase += 2.0f * data->freq / 44100.0f;
+
         /* When signal reaches top, drop back down. */
         if (data->phase >= 1.0f)
             data->phase -= 2.0f;
@@ -47,22 +50,22 @@ int main(void)
     data1.phase = 0.0f;
     data2.phase = 0.0f;
 
-    data1.freq  = 175;
+    data1.freq  = 50;
     data2.freq  = 100;
 
     /* Initialize library before making any other calls. */
     Pa_Initialize();
-    Pa_OpenDefaultStream(&stream1, 0, 1, paFloat32, 44100, 256, sawCallback, &data1);
-    Pa_OpenDefaultStream(&stream2, 0, 1, paFloat32, 44100, 256, sawCallback, &data2);
-        Pa_StartStream(stream1);
-    Pa_Sleep(2 * 1000);
-    Pa_StartStream(stream2);
-    Pa_Sleep(2 * 1000);
+    Pa_OpenDefaultStream(&stream1, 0, 2, paFloat32, 44100, 256, sawCallback, &data1);
+    //Pa_OpenDefaultStream(&stream2, 0, 2, paFloat32, 44100, 256, sawCallback, &data2);
+    Pa_StartStream(stream1);
+    Pa_Sleep(5 * 1000);
+    // Pa_StartStream(stream2);
+    // Pa_Sleep(2 * 1000);
     Pa_StopStream(stream1); 
     Pa_CloseStream(stream1); 
-    Pa_Sleep(2 * 1000);
-    Pa_StopStream(stream2); 
-    Pa_CloseStream(stream2); 
+    // Pa_Sleep(2 * 1000);
+    // Pa_StopStream(stream2); 
+    // Pa_CloseStream(stream2); 
 
     Pa_Terminate();
     return 0;
