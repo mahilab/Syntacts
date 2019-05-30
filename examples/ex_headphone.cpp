6c5a4b7968e7ca11aae081426240cb5da0b62945
Brandon T Cambio <btc6@ad.rice.edu>
2019-05-30 13:30:55 -0500

    cleaned file structure

---
+++ examples/ex_headphone.cpp
@@ -0,0 +1,112 @@
+#include <TactorFX/TactorFX.hpp>
+#include <MEL/Communications/MelShare.hpp>  // for plotting
+#include <MEL/Devices/Windows/Keyboard.hpp> // for keyboard
+#include <MEL/Utility/System.hpp>           // for sleep()
+#include <MEL/Math/Random.hpp>              // for random
+#include <MEL/Core/Console.hpp>             // for print
+#include <MEL/Core/Clock.hpp>
+#include <array>
+
+typedef mel::Keyboard KB;
+using mel::Key;
+using mel::sleep;
+using mel::MelShare;
+using mel::print;
+using mel::random;
+
+#define NUM_CH 2
+#define DEVICE 16
+
+using namespace tfx;
+
+int main(int argc, char const *argv[])
+{
+    tfx::initialize(DEVICE, NUM_CH);
+    DeviceInfo info = tfx::getCurrentDevice();
+    mel::print("Device Name: ", info.name);
+    mel::print("Device Index: ", info.index);
+    mel::print("Max Channels: ",info.maxChannels);
+
+    mel::Clock kbClock;
+
+    std::array<Key, NUM_CH> chn_keys = {Key::Num1, Key::Num2};    
+    while (!KB::is_key_pressed(Key::Escape)) {
+        if (KB::is_key_pressed(Key::L)) {
+            if (kbClock.get_elapsed_time() > mel::milliseconds(250)) {
+                    tfx::getAvailableDevices();
+                    kbClock.restart();
+            }
+        }
+
+        else if (KB::is_key_pressed(Key::B) && kbClock.get_elapsed_time() > mel::milliseconds(250)) {
+            auto osc = std::make_shared<SineWave>(175.0f, 0.05f);
+            // auto mod = std::make_shared<SineWave>(25.0f,1.0f);
+            auto env = std::make_shared<ASR>(1.f, 1.f, 1.f);
+            auto cue = std::make_shared<Cue>(osc, env);
+
+            for (int i = 1; i < 2; ++i)
+                tfx::playCue(i, cue);
+
+            kbClock.restart();
+        }
+
+        if (KB::is_key_pressed(Key::U) && kbClock.get_elapsed_time() > mel::milliseconds(250)) {
+            tfx::playCue(0, 1, 175, 1, 0, 0, 0, 0, 0.5f, 0);
+            kbClock.restart();
+        }
+
+        for (int ch = 0; ch < NUM_CH; ++ch) {
+            if (KB::is_key_pressed(chn_keys[ch])) {
+                float freq     = 175;  // 175 for Evan's tactors
+                float amp      = 0.05f; 
+                float dur      = 2.0f;
+                float a_time   = 1.0f;
+                float s_time   = 1.0f;
+                float r_time   = 1.0f;
+                float mod_freq = 0;
+                if (kbClock.get_elapsed_time() > mel::milliseconds(250)) {
+                    if (KB::is_key_pressed(Key::S)) {
+                        // just a sinwave for dur time
+                        auto osc = std::make_shared<SquareWave>(freq, amp);
+                        auto cue = std::make_shared<Cue>(osc, dur);  
+                        tfx::playCue(ch, cue);
+                    }                
+                    else if (KB::is_key_pressed(Key::W)) {
+                        // a sawave with an AS envelope
+                        auto osc = std::make_shared<SineWave>(freq, amp);
+                        auto env = std::make_shared<ASR>(a_time, s_time, r_time); 
+                        auto cue = std::make_shared<Cue>(osc, env);                
+                        tfx::playCue(ch, cue);
+                    }
+                    else if (KB::is_key_pressed(Key::C)) {
+                        // chaining
+                        auto cue = std::make_shared<Cue>();
+                        cue->chain<SquareWave>(freq, amp);
+                        cue->chain<SineWave>(mod_freq, amp);
+                        cue->chain<ASR>(a_time, s_time, r_time);
+                        tfx::playCue(ch, cue);
+                    }
+                    else if (KB::is_key_pressed(Key::Q)) {
+                        // chaining
+                        auto cue = std::make_shared<Cue>();
+                        cue->chain<SquareWave>(freq, amp);
+                        cue->chain<ASR>(a_time, s_time, r_time);
+                        tfx::playCue(ch, cue);
+                    }
+                    else if (KB::is_key_pressed(Key::T)) {
+                        // chaining
+                        auto cue = std::make_shared<Cue>();
+                        cue->chain<TriWave>(freq, amp);
+                        cue->chain<ASR>(a_time, s_time, r_time);
+                        tfx::playCue(ch, cue);
+                    }                    
+                    kbClock.restart();
+                }
+            }
+        }
+    }      
+
+    tfx::finalize();
+
+    return 0;
+}
---
+++ gui/tfx-gui_2ch.cpp
@@ -0,0 +1,208 @@
+// #define CARNOT_NO_CONSOLE
+// #define CARNOT_USE_DISCRETE_GPU
+
+#include <carnot>
+#include <TactorFX/TactorFX.hpp>
+#include <deque>
+#include <functional>
+
+#define NUM_CH 2
+#define DEVICE 4
+#define WIDTH 640
+#define HEIGHT 480
+
+using namespace carnot;
+
+//==============================================================================
+// WAVEFORM DRAWING FUNCTIONS
+//==============================================================================
+
+float sinWave(float f, float t) {
+    return std::sin(2.0f * Math::PI * f *t);
+}
+
+float sqrWave(float f, float t) {
+    return sinWave(f,t) > 0 ? 1.0f : -1.0f;
+}
+
+float sawWave(float f, float t) {
+   return -2.0f / Math::PI * std::atan(std::cos(Math::PI * f * t) / std::sin(Math::PI * f * t));
+}
+
+float triWave(float f, float t) {
+    return 2.0f / Math::PI * std::asin(std::sin(2.0f * Math::PI * f * t));
+}
+
+std::vector<std::function<float(float,float)>> waveFuncs = {sinWave, sqrWave, sawWave, triWave};
+
+//==============================================================================
+// DEMO GAMEOBJECT
+//==============================================================================
+
+class TfxDemo : public GameObject {
+public:
+
+    /// Constructor
+    TfxDemo() : chCB(NUM_CH, false), plot() { 
+        plot.reserve(44100*10);
+        chCB[0] = true;
+        tfx::initialize(DEVICE, NUM_CH);
+    }
+
+    /// Destructor
+    ~TfxDemo() {
+        tfx::finalize();
+    }
+
+    /// Creats and plays the user's cue
+    void play(std::size_t ch) {
+        Ptr<tfx::Oscillator> osc;
+        if (freq_wave == 0)
+            osc = make<tfx::SineWave>((float)freq, freq_amp);
+        else if (freq_wave == 1)
+            osc = make<tfx::SquareWave>((float)freq, freq_amp);
+        else if (freq_wave == 2)
+            osc = make<tfx::SawWave>((float)freq, freq_amp);
+        else if (freq_wave == 3)
+            osc = make<tfx::TriWave>((float)freq, freq_amp);            
+        auto env = make<tfx::ASR>(a/1000.0f, s/1000.0f, r/1000.f);
+        Ptr<tfx::Cue> cue;
+        if (mod > 0) {
+            Ptr<tfx::Oscillator> modOsc;
+            if (mod_wave == 0)
+                modOsc = make<tfx::SineWave>((float)mod, 1.0f);
+            else if (mod_wave == 1)
+                modOsc = make<tfx::SquareWave>((float)mod, mod_amp);
+            else if (mod_wave == 2)
+                modOsc = make<tfx::SawWave>((float)mod, mod_amp);
+            else if (mod_wave == 3)
+                modOsc = make<tfx::TriWave>((float)mod, mod_amp);  
+            cue = make<tfx::Cue>(osc, modOsc, env);
+        }
+        else
+            cue = make<tfx::Cue>(osc, env);
+        tfx::playCue((int)ch, cue);
+    }
+
+    /// Overrides GameObject::update to draw GUI and get user inputs
+    void update() override {    
+        ImGui::SetNextWindowPos(Vector2f(5,5), ImGuiCond_Always);
+        ImGui::SetNextWindowSize(Vector2f(WIDTH-10,HEIGHT-10), ImGuiCond_Always);
+        ImGui::Begin("TactorFX", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);   
+        //------------------------------------------------------------------
+        if (ImGui::Button(ICON_FA_PLAY) || Input::getKeyDown(Key::Space)) {
+            for (std::size_t i = 0; i < NUM_CH; ++i) {
+                if (chCB[i])
+                    play(i);
+            }
+        }
+        ImGui::SameLine();
+        if (ImGui::Button(ICON_FA_STOP))
+            tfx::stopAllCues();
+        ImGui::SameLine();
+        // if (ImGui::Button(ICON_FA_SYNC_ALT)) {
+        //     tfx::finalize();
+        //     tfx::initialize(NUM_CH);
+        // }
+        ImGui::SameLine();
+        ImGui::Text(tfx::getCurrentDevice().name.c_str());
+        ImGui::Separator();
+        //------------------------------------------------------------------
+        for (std::size_t i = 0; i < NUM_CH; ++i)
+        {
+            auto label = str("Ch",i+1);
+            if (ImGui::Button(label.c_str()) || Input::getKeyDown((Key)((int)Key::Num1 + i)))
+                play(i);
+            ImGui::SameLine();
+            ImGui::Checkbox(str("##",i).c_str(), &chCB[i]);
+
+            if (((i+1) % NUM_CH) != 0)
+                ImGui::SameLine();
+        }
+        ImGui::Separator();
+        //------------------------------------------------------------------
+        ImGui::Text("Carrier");
+        ImGui::RadioButton("Sine##C", &freq_wave, 0);   ImGui::SameLine();
+        ImGui::RadioButton("Square##C", &freq_wave, 1); ImGui::SameLine();
+        ImGui::RadioButton("Saw##C", &freq_wave, 2);    ImGui::SameLine();
+        ImGui::RadioButton("Triangle##C", &freq_wave, 3);
+        ImGui::DragInt("Frequency##C", &freq, 0.5f, 0, 250, "%i Hz");
+        ImGui::DragFloat("Amplitude##C", &freq_amp, 0.005f, 0.0f, 1.0f);
+        ImGui::Separator();
+        //------------------------------------------------------------------
+        ImGui::Text("Modulation");
+        ImGui::RadioButton("Sine##M", &mod_wave, 0);   ImGui::SameLine();
+        ImGui::RadioButton("Square##M", &mod_wave, 1); ImGui::SameLine();
+        ImGui::RadioButton("Saw##M", &mod_wave, 2);    ImGui::SameLine();
+        ImGui::RadioButton("Triangle##M", &mod_wave, 3);
+        ImGui::DragInt("Frequency##M", &mod, 0.5f,  0, 250, "%i Hz");
+        ImGui::DragFloat("Amplitude##M", &mod_amp, 0.005f, 0.0f, 1.0f);
+        ImGui::Separator();
+        //------------------------------------------------------------------
+        ImGui::Text("Envelope");
+        ImGui::DragInt("Attack", &a, 0.5f,  0, 1000, "%i ms");
+        ImGui::DragInt("Sustain", &s, 0.5f,  0, 1000, "%i ms");
+        ImGui::DragInt("Release", &r, 0.5f,  0, 1000, "%i ms");
+        ImGui::Separator();
+        //------------------------------------------------------------------
+        plot.clear();
+        int durMs = a+s+r;
+        float durS = (float)durMs / 1000.0f;
+        int samples = (int)(durS * 44100.0f);
+
+        for (int i = 0; i < samples; ++i) {
+            float t = durS / (float)samples * (float)i;
+            float v = 1.0f;
+            v *= freq_amp * waveFuncs[freq_wave]((float)freq, t);
+            // modulation
+            if (mod > 0)
+                v*= mod_amp * waveFuncs[mod_wave]((float)mod, t);
+            // envelope
+            float env = 1.0f;
+            if (t < (float)a/1000.0f)
+                env = Math::interp(t,0.0f,(float)a/1000.0f,0.0f,1.0f);
+            else if (t > (float)(a+s)/1000.0f)
+                env = Math::interp(t, (float)(a+s)/1000.0f, (float)(a+s+r)/1000.0f, 1.0f, 0.0f);
+            v *= env;
+
+            plot.push_back(v);
+        }
+        ImGui::PushItemWidth(615);
+        ImGui::PushStyleColor(ImGuiCol_PlotLines, hexCode("cf94c2"));
+        ImGui::PushStyleColor(ImGuiCol_PlotLinesHovered, hexCode("cf94c2"));
+        auto title = str(durMs, "ms /", samples, " samples");
+        ImGui::PlotLines("", &plot[0], (int)plot.size(),  0, title.c_str(), -1.0f, 1.0f, ImVec2(0,104));
+        ImGui::PopStyleColor();
+        ImGui::PopStyleColor();
+        ImGui::PopItemWidth();
+        ImGui::End();
+    }
+
+private:
+
+    int   freq = 175;
+    float freq_amp  = 0.5f;
+    int   freq_wave = 0;
+
+    int   mod = 25;
+    float mod_amp = 1.0f;
+    int   mod_wave = 0;
+
+    int a = 50;
+    int s = 75;
+    int r = 25;
+
+    std::deque<bool> chCB;
+    std::vector<float> plot;
+};
+
+
+int main(int argc, char const *argv[])
+{
+    /// Initialize and run Engine
+    Engine::init(WIDTH, HEIGHT, WindowStyle::Close);
+    Engine::window->setTitle("TactorFX");
+    Engine::makeRoot<TfxDemo>();
+    Engine::run();  
+    return 0;
+}
