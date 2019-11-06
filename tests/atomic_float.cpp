b8795f4e7d634c85cb05731511bf9896e497a3c6
Evan Pezent <epezent@rice.edu>
2019-11-06 13:23:49 -0600

    more invest

--- tests/atomic_float.cpp
+++ tests/atomic_float.cpp
@@ -4,11 +4,29 @@
 #include <thread>
 #include <carnot>
 #include <Syntacts/Syntacts.hpp>
+#include <fstream>
 
 using namespace tact;
 
-class SW : public Source {
+template <typename T>
+int exportToCsv(T& sampler, std::string filePath, int sampleRate = 44100) {
+    std::ofstream s;
+    s.open(filePath);
+    int n = 352800;
+    std::cout << n << std::endl;
+    float sampleLength = 1.0f / sampleRate;
+    float t = 0;
+    for (std::size_t i = 0; i < n; ++i) {
+        float sample = sampler->sample(t);
+        s << sample << std::endl;
+        t += sampleLength;
+    }
+    s.close();
+    return 0;
+}
 
+class SW : public Source {
+public:
     float sample(float t) const override {
         return sin(6.28318530717959f * 880 * t);
     }
@@ -22,11 +40,15 @@ int main(int argc, char const *argv[])
     auto mod = create<SineWave>(5);
     auto env = create<ASR>(0.001,5,0.001);
     auto cue = create<Cue>();
-    cue->setEnvelope(create<AmplitudeEnvelope>(5,1));
-    cue->chain(create<SW>());
+
+    auto benv = create<AmplitudeEnvelope>(8,0.5);
+    cue->setEnvelope(benv);
+    auto sw = create<SW>();
+    cue->chain(sw);
 
     tact::play(0, cue);
     exportToWave(cue, "cue.wav");
+    exportToCsv(cue, "cue.csv");
 
     carnot::Clock clock;
     while (clock.getElapsedTime().asSeconds() < cue->getEnvelope()->getDuration()) {
