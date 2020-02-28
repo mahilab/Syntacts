14f0864e0db253554d49106353c1952455f88230
epezent <epezent@rice.edu>
2020-02-27 18:16:46 -0600

    fix compile issue on macos

--- gui/src/StatusBar.cpp
+++ gui/src/StatusBar.cpp
@@ -8,10 +8,11 @@ using namespace mahi::gui;
 
 void StatusBar::pushMessage(const std::string &text, InfoLevel level)
 {
+    Color textCol = ImGui::GetStyle().Colors[ImGuiCol_Text];
     m_fadeTime = 0;
     m_fadeColors[0.00f] = {0,0,0,0};
-    m_fadeColors[0.02f] = level == InfoLevel::Info ? ImGui::GetStyle().Colors[ImGuiCol_Text] : level == Warning ? Yellows::Khaki : Reds::LightCoral;
-    m_fadeColors[0.80f] = level == InfoLevel::Info ? ImGui::GetStyle().Colors[ImGuiCol_Text] : level == Warning ? Yellows::Khaki : Reds::LightCoral;
+    m_fadeColors[0.02f] = level == InfoLevel::Info ? textCol : level == Warning ? Yellows::Khaki : Reds::LightCoral;
+    m_fadeColors[0.80f] = level == InfoLevel::Info ? textCol : level == Warning ? Yellows::Khaki : Reds::LightCoral;
     m_fadeColors[1.00f] = {0,0,0,0};
     m_notification = text;
 }
